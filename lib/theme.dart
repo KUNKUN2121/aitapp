99e0c4871cf19864db2947cdf6c3ffb3dbce8bbe
piman528 <youhei0528.0722@gmail.com>
2024-04-26 07:13:38 +0900

    uiの色調整

--- lib/theme.dart
+++ lib/theme.dart
@@ -2,23 +2,25 @@ import 'package:flutter/material.dart';
 
 ColorScheme kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 35, 132, 223),
-  background: const Color.fromARGB(255, 240, 247, 255),
+  background: const Color.fromARGB(255, 248, 248, 248),
   secondary: const Color.fromARGB(255, 52, 76, 102),
-  primaryContainer: const Color.fromARGB(255, 221, 231, 236),
+  primaryContainer: const Color.fromARGB(255, 232, 232, 232),
+  onPrimaryContainer: Colors.black,
   secondaryContainer: const Color.fromARGB(255, 188, 218, 247),
   tertiaryContainer: const Color.fromARGB(255, 63, 157, 240),
-  onTertiaryContainer: const Color.fromARGB(255, 217, 232, 247),
+  onTertiaryContainer: const Color.fromARGB(255, 252, 252, 252),
 );
 
 ColorScheme kDarkColorScheme = ColorScheme.fromSeed(
   brightness: Brightness.dark,
   seedColor: const Color.fromARGB(255, 35, 132, 223),
   secondary: const Color.fromARGB(255, 148, 181, 218),
-  background: const Color.fromARGB(255, 20, 26, 32),
-  primaryContainer: const Color.fromARGB(255, 32, 43, 54),
+  background: const Color.fromARGB(255, 27, 27, 27),
+  primaryContainer: const Color.fromARGB(255, 43, 43, 43),
+  onPrimaryContainer: const Color.fromARGB(255, 255, 255, 255),
   secondaryContainer: const Color.fromARGB(255, 46, 82, 114),
   tertiaryContainer: const Color.fromARGB(255, 75, 111, 143),
-  onTertiaryContainer: const Color.fromARGB(255, 217, 232, 247),
+  onTertiaryContainer: const Color.fromARGB(255, 255, 255, 255),
 );
 
 ThemeData buildThemeLight() {
@@ -41,6 +43,9 @@ ThemeData buildThemeDark() {
     dividerColor: const Color.fromARGB(255, 59, 59, 59),
     appBarTheme: AppBarTheme(
       backgroundColor: kDarkColorScheme.background,
+    ),
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      backgroundColor: Color.fromARGB(255, 34, 34, 34),
     ),
   );
 }
