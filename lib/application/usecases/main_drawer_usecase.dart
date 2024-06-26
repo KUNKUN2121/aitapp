2d748bb62598c5be1bf9f0426a2984b9d763d4e8
piman <youhei0528.0722@gmail.com>
2024-06-26 09:52:10 +0900

    ログイン処理を変更

--- lib/application/usecases/main_drawer_usecase.dart
+++ lib/application/usecases/main_drawer_usecase.dart
@@ -36,8 +36,8 @@ class MainDrawerUseCase {
     final pref = ref.read(sharedPreferencesProvider);
     await pref.remove('id');
     await pref.remove('password');
-    await _replaceGo(widget);
     ref.read(identityProvider.notifier).clear();
+    await _replaceGo(widget);
   }
 
   Future<void> loginCampus({
